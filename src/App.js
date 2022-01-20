a5260e63f2757c1e3a2d002bd218e43aa27c99e0
rutekmar00 <46930922+rutekmar00@users.noreply.github.com>
2022-01-20 11:45:08 +0100

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App">Weather forecast application</div>;
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Weather Forecast Application</h1>
+    </header>
+  );
+}
